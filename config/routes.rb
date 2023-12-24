6545e9e894da4307382fdc816e85847e6568f49f
“sadaf <sadaf.batekzai54321@gmial.com>
2023-12-24 21:24:12 +0430

    add style into login and sign up pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root to: "users#index"
+  root to: "home#index"
   # Defines the root path route ("/")
   # root "posts#index"
 end
