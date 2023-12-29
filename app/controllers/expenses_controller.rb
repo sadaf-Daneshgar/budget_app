4e3ee25711fffd0a95cb65cb6b8f5d3acb6ce6df
“sadaf <sadaf.batekzai54321@gmial.com>
2023-12-29 11:32:40 +0430

    fix N+1 problem

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,7 +2,7 @@ class ExpensesController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @category = Category.find(params[:category_id])
-    @expenses = @user.expenses.where(category: @category)
+    @expenses = @user.expenses.includes(:category).where(category: @category)
   end
 
   def new
