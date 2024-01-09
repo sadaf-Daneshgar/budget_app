1456ee40b578f0f3b263af80dd5ce893df73b219
“sadaf <sadaf.batekzai54321@gmial.com>
2024-01-09 18:04:21 +0430

    add the files and codes that needed for deploying

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_many :categories_expenses, dependent: :destroy
-  has_many :expenses, through: :categories_expenses
+  has_many :expenses, through: :categories_expenses, dependent: :destroy
 
   validates :name, presence: true
   validates :icon, presence: true
